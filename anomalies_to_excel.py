c1482c4c7520158aab02b7a9e915b36187e7419e
pablomathamel <71036815+pablomathamel@users.noreply.github.com>
2023-11-06 12:25:37 +0100

    Update anomalies_to_excel.py

--- anomalies_to_excel.py
+++ anomalies_to_excel.py
@@ -36,11 +36,12 @@ def get_anomalies(auth_token):
 
    return requests.get(url,headers=headers, verify=False).json()
 
-
-
+# Store anomalies in a python dictionary.
 anomalies_list=get_anomalies(get_token())
+
 all_anomalies=[]
 
+#Iterate through the anomalies dictionary, and construct a list with the relevant fields of each anomaly
 for A in anomalies_list["entries"]:
     single_anomaly=[]
     single_anomaly.append(A["anomalyReason"])
@@ -62,7 +63,7 @@ for A in anomalies_list["entries"]:
 df = pd.DataFrame(all_anomalies, columns=["What's wrong","Anomaly Level","Category","Site","Detection Time","Title","Nodes","Status","Last Seen Time","Cleared"])
 
 # Specify the name of the Excel file
-excel_file = './anomalies_list.xlsx'  # Replace 'data.xlsx' with the desired Excel file name
+excel_file = './anomalies_list.xlsx'
 
 # Export the data to an Excel file
 df.to_excel(excel_file, index=False)
